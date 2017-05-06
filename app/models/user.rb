6fdfa5b1d7554c2fc559f53ac0c9245c5e4029f3
Shuya Lei <syz_ll@outlook.com>
2017-05-07 01:09:26 +0800

    add admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-    is_admin
+    email=='fullstack@163.com'
   end
 
   has_many :resumes
